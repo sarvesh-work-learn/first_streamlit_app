f361f1d3952263f1b1af83ab46c7a454c10616b0
sarvesh-work-learn <143883562+sarvesh-work-learn@users.noreply.github.com>
2023-09-02 18:33:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Heatlhy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text(Orance, Mango, Blueberry)
+streamlit.text('Orance, Mango, Blueberry')
