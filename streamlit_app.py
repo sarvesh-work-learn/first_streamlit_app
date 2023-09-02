a1ec0e090cd1dbb8aeddac38fd6a127883bc7c5d
sarvesh-work-learn <143883562+sarvesh-work-learn@users.noreply.github.com>
2023-09-02 18:40:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avacado')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
