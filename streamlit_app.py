d1c68e8c5445ec2f5e87c7795d18a15cd8e820fa
sarvesh-work-learn <143883562+sarvesh-work-learn@users.noreply.github.com>
2023-09-02 18:34:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-import streamlit
-streamlit.title('My Parents New Heatlhy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Orance, Mango, Blueberry')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
